563224a87cfebaffad656cc1b4c5a2c3e3228565
Piotr Balcer <piotr.balcer@intel.com>
2014-11-13 11:55:58 +0100

    basic key-value store example based on pmemobj.

---
+++ src/test/obj_kvstore/obj_kvstore.c
@@ -0,0 +1,84 @@
+#include "unittest.h"
+#include "libpmem.h"
+#include <assert.h>
+
+#define PMEM(x) PMEMoid
+
+typedef struct node_s node_t;
+
+struct node_s {
+	PMEM(node_t) leafs[127];
+	int value;
+};
+
+struct base {
+	PMEM(node_t) root;
+};
+
+void kvs_set(PMEMobjpool *pool, const char *key, int value) {
+	struct base *bp = pmemobj_root_direct(pool, sizeof (*bp));
+
+	pmemobj_tx_begin(pool, NULL);
+	int i;
+	PMEM(node_t) *node = &(bp->root);
+	node_t *dnode = NULL;
+	for (i = 0; i < strlen(key); ++i) {
+		dnode = pmemobj_direct(*node);
+		if (dnode == NULL) {
+			PMEM(node_t) nnode = pmemobj_zalloc(sizeof(node_t));
+			PMEMOBJ_SET(*node, nnode);
+			dnode = pmemobj_direct(*node);
+		}
+		node = &(dnode->leafs[(int)key[i]]);
+	}
+	pmemobj_memcpy(&dnode->value, &value, sizeof(int));
+
+	pmemobj_tx_commit();
+}
+
+void kvs_delete(PMEMobjpool *pool, const char *key) {
+	kvs_set(pool, key, 0);
+}
+
+int kvs_get(PMEMobjpool *pool, const char *key) {
+	struct base *bp = pmemobj_root_direct(pool, sizeof (*bp));
+	int i;
+	PMEM(node_t) node = bp->root;
+	node_t *dnode;
+	for (i = 0; i < strlen(key); ++i) {
+		dnode = pmemobj_direct(node);
+		if (dnode == NULL) {
+			break;
+		}
+		node = dnode->leafs[(int)key[i]];
+	}
+
+	return dnode != NULL ? dnode->value : 0;
+}
+
+int main(int argc, char** argv)
+{
+	START(argc, argv, "obj_kvstore");
+
+	if (argc < 2)
+		FATAL("usage: %s file", argv[0]);
+
+	PMEMobjpool *pop = pmemobj_pool_open(argv[1]);
+	kvs_set(pop, "AaAaA", 1);
+	assert(kvs_get(pop, "AaAaA") == 1);
+	kvs_set(pop, "aAaAa", 2);
+	assert(kvs_get(pop, "aAaAa") == 2);
+	kvs_set(pop, "ABCDE", 3);
+	assert(kvs_get(pop, "ABCDE") == 3);
+	kvs_set(pop, "abcde", 4);
+	assert(kvs_get(pop, "abcde") == 4);
+	kvs_set(pop, "123456", 5);
+	assert(kvs_get(pop, "123456") == 5);
+	kvs_set(pop, "1234567", 6);
+	assert(kvs_get(pop, "1234567") == 6);
+	/* all done */
+	pmemobj_pool_close(pop);
+
+	DONE(NULL);
+}
+
